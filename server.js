58adc3165b82fcaa6690bb4a24082525efc76fd4
PravallikaChintapalli <21pa1a0434@vishnu.edu.in>
2025-09-19 19:06:45 +0530

    server.js

--- server.js
+++ server.js
@@ -2,27 +2,29 @@ import express from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-// __dirname replacement in ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const initial_path = path.join(__dirname, "public");
 
-app.use(express.static(initial_path));
+// Serve static files from project root instead of /public
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(initial_path, "home.html"));
+    res.sendFile(path.join(__dirname, "home.html"));
 });
 
 app.get('/editor', (req, res) => {
-    res.sendFile(path.join(initial_path, "editor.html"));
+    res.sendFile(path.join(__dirname, "editor.html"));
 });
 
-app.get("/blog", (req, res) => {
-    res.sendFile(path.join(initial_path, "blog.html"));
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, "blog.html"));
 });
 
-app.listen(3000, () => {
-    console.log('Server listening on http://localhost:3000');
+// Use Render's PORT variable
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
+
